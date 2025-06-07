42d72ddf58a2b3b1ccda019404deaaf8af748156
shaheer1642 <shaheer1642@gmail.com>
2025-06-07 06:57:11 +0500

    session 2

---
+++ basic-building-blocks/input-output.js
@@ -0,0 +1,6 @@
+// input/output
+console.log('hello world');
+console.error('something went wrong');
+console.warn('something went wrong, but no need to worry about it')
+
+// prompt('enter a value') // would not work in node
